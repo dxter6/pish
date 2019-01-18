7fae12a14d331c5100a75c4930927aeb050dacb9
Shinu <36124884+Shinu007@users.noreply.github.com>
2019-01-18 18:01:12 +0530

    Update PISH.py

--- PISH.py
+++ PISH.py
@@ -1,5 +1,5 @@
 import os, bs4, requests
-b = input("enter the site")
+b = input("enter the site: ")
 a = int(input("""enter the Attacks(1::Raw content,2::page-status-code,3::pretty-Html,4::clones the website in your desktop):"""))
 page = requests.get(b)
 from bs4 import BeautifulSoup
