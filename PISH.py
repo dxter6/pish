c08b3f2e5ad1c4965177c4623c5fd61f7b143b1c
shinu007 <36124884+shinu007@users.noreply.github.com>
2019-01-18 16:53:40 +0530

    Add files via upload

---
+++ PISH.py
@@ -0,0 +1,16 @@
+import os, bs4, requests
+b = input("enter the site")
+a = int(input("""enter the Attacks(1::Raw content,2::page-status-code,3::pretty-Html):"""))
+page = requests.get(b)
+from bs4 import BeautifulSoup
+soup = BeautifulSoup(page.content, 'html.parser')
+if a == 1:
+    print(page.content)
+if a == 2:
+    print(page.status_code)
+if a == 3:
+    print(soup.prettify())
+if a == 4:
+    Html_file=open("hello.html","w")
+    Html_file.write(soup.prettify())
+    Html_file.close
